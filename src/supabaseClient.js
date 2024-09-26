0997399b218485da5ee52ef298dbb131c4efb694
SantiagoZambon <zambonsantiago@gmail.com>
2024-09-26 19:27:47 -0300

    API de supabase cargada en el proyecto

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-supabase-url.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
+const supabaseUrl = 'https://hspmxytfbywljwkydlpi.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzcG14eXRmYnl3bGp3a3lkbHBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcwNDY1MDcsImV4cCI6MjA0MjYyMjUwN30.ToyvVv5pnlJwwGVIdMzJnyFky6YRzICBzBz6pgzxl-s';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
